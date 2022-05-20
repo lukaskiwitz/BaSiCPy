82c6d61deb8bbcdbe4d4c01faa5c68351c65cbf0
Lukas <kiwitzlu@hu-berlin.com>
2022-05-20 18:59:38 +0200

    fixed circular import

--- pybasic/__init__.py
+++ pybasic/__init__.py
@@ -6,7 +6,7 @@ Created on Thu Dec  5 17:32:25 2019
 @author: Mohammad Mirkazemi
 """
 
-from . import tools
+from pybasic import tools
 from ._background import background_timelapse, basic, correct_illumination
 from ._settings import PyBasicConfig
 settings = PyBasicConfig()
