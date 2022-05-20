f03f738a532621297a65d34fa606d091f62ba011
Lukas <kiwitzlu@hu-berlin.com>
2022-05-20 18:59:38 +0200

    fixed circular import

--- setup.py
+++ setup.py
@@ -9,6 +9,6 @@ setup(
     install_requires=[
         l.strip() for l in Path('requirements.txt').read_text('utf-8').splitlines()
     ],
-    packages=[ 'pybasic']
+    packages=[ 'pybasic','pybasic.tools']
 
 )
